8c65df173d3033afa4186abfb164133cc1f95ede
Jeanderson Candido <jeandersonbc@gmail.com>
2016-06-10 17:09:13 -0300

    Implemented auxiliary script to analyze dependencies among tests

--- feasibility-study/run-tests.sh
+++ feasibility-study/run-tests.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 BASEDIR="`pwd`"
-OUTPUT_DIR="$BASEDIR/reports"
-ANALYZER_JAR="dchecker-0.1.1-SNAPSHOT.jar"
-
-if [ -f "$ANALYZER_JAR" ]; then
-    echo " > Error: Analyzer Jar not found. Build \"dchecker\" first..."
-    echo " > Aborted."
-    exit 1;
-fi
+ANALYZER_JAR="dchecker-0.0.1-SNAPSHOT.jar"
 
 TEST_PATH="$1"
+NAME="$2"
 if [ -z "$TEST_PATH" ]; then
     echo " > Usage: ./run-tests <path to tests>"
     exit 1;
 fi
 
-timestamp_report() {
-    cat "$OUTPUT_DIR/$1" | grep INFO: | sed "s/INFO: //g"
+create_timestamp_report() {
+    cat "$1" | grep INFO: | sed "s/INFO: //g"
 }
 
-NAME="$2"
 LOGPREFFIX="`date +"$NAME-%m%d%H%M%S"`"
+OUTPUT_DIR="$BASEDIR/reports"
+
+# Output files
+MVNLOG_FILE="$OUTPUT_DIR/$LOGPREFFIX-mvnlog.txt"
+TIMESTAMP_FILE="$OUTPUT_DIR/$LOGPREFFIX-ts.csv"
+DEPENDENCY_FILE="$OUTPUT_DIR/$LOGPREFFIX-deps.txt"
+
 [[ ! -d "$OUTPUT_DIR" ]] && mkdir "$OUTPUT_DIR"
 
 cd "$TEST_PATH"
-
-mvn test &> "$OUTPUT_DIR/$LOGPREFFIX-mvnlog.txt"
-timestamp_report "$LOGPREFFIX-mvnlog.txt" > "$OUTPUT_DIR/$LOGPREFFIX-ts.csv"
-
-#FIXME: Error: Unable to access jarfile dchecker-0.1.1-SNAPSHOT.jar
-java -jar "$ANALYZER_JAR" "$OUTPUT_DIR/$LOGPREFFIX-ts.csv" > "$OUTPUT_DIR/$LOGPREFFIX-deps.txt"
-
+mvn test &> $MVNLOG_FILE
+create_timestamp_report $MVNLOG_FILE > $TIMESTAMP_FILE
 cd "$BASEDIR"
+
+if [ ! -f "$ANALYZER_JAR" ]; then
+    echo " > Error: Analyzer jar file \"$ANALYZER_JAR\" not found in the current directory"
+    echo " > Aborted."
+    exit 1;
+fi
+java -jar "$ANALYZER_JAR" "$TIMESTAMP_FILE" > "$DEPENDENCY_FILE"
