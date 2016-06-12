3aa0d8f3e0abe7caeef6d0901f9a2741452bf178
Jeanderson Candido <jeandersonbc@gmail.com>
2016-06-12 18:40:38 -0300

    Added Retrofit

--- feasibility-study/setup-all.sh
+++ feasibility-study/setup-all.sh
@@ -22,12 +22,15 @@ function download_git {
 
 # Ref: https://travis-ci.org/graphhopper/graphhopper/builds/131435929
 download_git "https://github.com/graphhopper/graphhopper.git" "graphhopper" "a5bfe93dc5d13bdd86c47f5b7fdacabf034a6cd3"
+download_git "https://github.com/square/retrofit.git" "retrofit" "28d350d99430c87b4ada7d1aa9e08c96884cb388"
 
 # Commented subjects were already executed and results should be available
 # in the dropbox folder.
 #
 #./run_mvn_experiment.sh "subjects/retrofit" "retrofit" > retrofit-summary.txt
 ./setup.sh "subjects/graphhopper" "core"
+./setup.sh "subjects/retrofit" "retrofit"
+
 #./run_mvn_experiment.sh "subjects/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
 #./run_mvn_experiment.sh "subjects/camel/camel-core" > camel-core-summary.txt
 #./run_mvn_experiment.sh "subjects/jetty.project" "jetty-client" > jetty.project-summary.txt
