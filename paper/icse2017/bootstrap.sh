910eb0841a9b9214decc614b17dfed018b5ee883
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-05 18:15:07 -0300

    Added bootstrap script to install required tools

---
+++ paper/icse2017/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#
+# This script installs required tools to generate the final
+# PDF file.
+#
+# Author: Jeanderson Candido
+
+# Building tool
+sudo apt-get install latexmk
+
+# Installs missing fonts
+sudo apt-get install texlive-fonts-recommended
