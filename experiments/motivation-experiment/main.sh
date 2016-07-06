b3ab029d69ad7d5d2d020d5dc64577620a61a4d1
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-06 09:07:47 -0300

    Updated main.sh

--- experiments/motivation-experiment/main.sh
+++ experiments/motivation-experiment/main.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 ../scripts/download_subjects.sh
 
-RUNS=50
+RUNS=$1
+[[ -z $RUNS ]] && echo "Usage: ./main.sh <RUNS>" && exit 1
 
-# Commented subjects were already executed and results should be available
-# in the dropbox folder.
-#
-# ./experiment.sh $RUNS "subjects/retrofit" "retrofit" > retrofit-summary.txt
+./experiment.sh $RUNS "subjects/retrofit" "retrofit" > retrofit-summary.txt
+
+# Possibly with deadlock when executed with parallel threads
 # ./experiment.sh $RUNS "subjects/graphhopper" "core" > graphhopper-summary.txt
+
 ./experiment.sh $RUNS "subjects/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
-# ./experiment.sh $RUNS "subjects/camel/camel-core" > camel-core-summary.txt
+./experiment.sh $RUNS "subjects/camel/camel-core" > camel-core-summary.txt
 ./experiment.sh $RUNS "subjects/jetty.project" "jetty-client" > jetty.project-summary.txt
-# ./experiment.sh $RUNS "subjects/okhttp" "okhttp-tests" > okhttp-summary.txt
+./experiment.sh $RUNS "subjects/okhttp" "okhttp-tests" > okhttp-summary.txt
