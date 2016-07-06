1e3fe084aae95b66c96c7c15858af8a1c06e50ed
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-06 17:29:17 -0300

    Subject directory should be a common directory among experiments

--- experiments/motivation-experiment/main.sh
+++ experiments/motivation-experiment/main.sh
@@ -10,17 +10,21 @@
 #
 # Author: Jeanderson Candido
 #
+SUBJECTS_HOME="../subjects"
 RUNS=$1
+
 [[ -z "$RUNS" ]] && echo "Usage: ./main.sh <RUNS>" && exit 1
+if [ ! -d "$SUBJECTS_HOME" ]; then
+    echo "Download subjects first before proceeding"
+    exit 1
+fi
 
-../scripts/download_subjects.sh
-
-./experiment.sh $RUNS "subjects/retrofit" "retrofit" > retrofit-summary.txt
+./experiment.sh $RUNS "$SUBJECTS_HOME/retrofit" "retrofit" > retrofit-summary.txt
 
 # Possibly with deadlock when executed with parallel threads
-# ./experiment.sh $RUNS "subjects/graphhopper" "core" > graphhopper-summary.txt
+# ./experiment.sh $RUNS "$SUBJECTS_HOME/graphhopper" "core" > graphhopper-summary.txt
 
-./experiment.sh $RUNS "subjects/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
-./experiment.sh $RUNS "subjects/camel/camel-core" > camel-core-summary.txt
-./experiment.sh $RUNS "subjects/jetty.project" "jetty-client" > jetty.project-summary.txt
-./experiment.sh $RUNS "subjects/okhttp" "okhttp-tests" > okhttp-summary.txt
+./experiment.sh $RUNS "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
+./experiment.sh $RUNS "$SUBJECTS_HOME/camel/camel-core" > camel-core-summary.txt
+./experiment.sh $RUNS "$SUBJECTS_HOME/jetty.project" "jetty-client" > jetty.project-summary.txt
+./experiment.sh $RUNS "$SUBJECTS_HOME/okhttp" "okhttp-tests" > okhttp-summary.txt
