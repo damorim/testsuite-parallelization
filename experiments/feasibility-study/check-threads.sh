e2711ff4d21e4c423f9d14f11d937b0685238f84
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-06 17:53:09 -0300

    Removed unnecessary scripts

--- experiments/feasibility-study/check-threads.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-FILE_PATH="$1"
-
-cat "$FILE_PATH" | grep pool- | sed "s/[0-9]) [0-9]*, [0-9]*, //g" | sed "s/,.*//g" | sort | uniq
-cat "$FILE_PATH" | grep pool- | sed "s/[0-9]) [0-9]*, [0-9]*, //g" | sed "s/,.*//g" | sort | uniq | wc -l
