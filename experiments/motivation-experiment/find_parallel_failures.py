d4654d8236d9b93efe31cc816c1db720530d5dd2
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-08 00:49:40 -0300

    Fixes #2 - Executing exhaustively with according to a threshold

--- experiments/motivation-experiment/find_parallel_failures.py
+++ experiments/motivation-experiment/find_parallel_failures.py
@@ -8,41 +8,58 @@ from sys import argv
 
 from utils import *
 
-freq = 10 #FIXME
+THRESHOLD = 5
 
 def find_parallel_failures(test_path, reports_dir, log_mvn):
     results = {}
+    failing_tests = []
 
     log_mvn = os.path.join(os.path.abspath(os.curdir), log_mvn + "-mvnlog.txt")
-    log_mvn = open(log_mvn, "w")
+    log_mvn = open(log_mvn, "a")
 
     curdir = os.path.abspath(os.curdir)
     os.chdir(test_path)
 
-    for i in range(freq):
+    reruns = 0
+    runs = 0
+    has_exausted = False
+    while (reruns < THRESHOLD):
         call(['mvn', 'test'], stderr=log_mvn, stdout=log_mvn)
         reports = reports_from(reports_dir)
+
+        # Assume this execution has exausted (ie no new failing tests discovered)
+        has_exausted =  True
         for xmlFile in reports:
             xmlFileName = os.path.join(reports_dir, xmlFile)
             e = xml.etree.ElementTree.parse(xmlFileName).getroot()
             for atype in e.findall('testcase'):
                 label = result_label(atype)
                 key = atype.get('classname') + '#' + atype.get('name')
+
+                # Check if this is a failing test
+                if label == FAIL_LABEL and not key in failing_tests:
+                    failing_tests.append(key)
+                    has_exausted = False
+                    reruns = 0
+
                 if key in results:
                     results[key].append(label);
                 else:
                     results[key] = [label]
 
+        runs += 1
+        if has_exausted:
+            reruns += 1
+
     log_mvn.close()
     os.chdir(curdir)
 
-    failing_tests = []
-    for test,r in results.items():
-        if 'F' in r:
-            failing_tests.append(test)
-            print test,r
+    print "Parallel Flaky Tests"
+    for test in failing_tests:
+        print " -",test,results[test]
 
     statistics = compute_statistcs(results)
+    print "[Runs]:", runs, "Threshold:" reruns
     print "[Statistics] All: {total}, Skipped: {skips}, Runs: {runs}, " \
           "Failed (any): {fails}, ".format(**statistics)
 
@@ -53,7 +70,7 @@ def check_failures_individually(test_path, reports_dir, output_log, failures):
     output = {}
 
     output_log = os.path.join(os.path.abspath(os.curdir), output_log + "-mvnlog.txt")
-    output_log = open(output_log, "w")
+    output_log = open(output_log, "a")
 
     curdir = os.path.abspath(os.curdir)
     os.chdir(test_path)
@@ -61,7 +78,7 @@ def check_failures_individually(test_path, reports_dir, output_log, failures):
     for test_case in failures:
         output[test_case] = []
         rmtree(reports_dir)
-        for f in range(freq):
+        for f in range(THRESHOLD):
             call(['mvn', '-Dtest=' + test_case, 'test'], stderr=output_log, stdout=output_log)
 
             xmlfile = report_from(reports_dir)
@@ -83,9 +100,10 @@ def check_failures_individually(test_path, reports_dir, output_log, failures):
     output_log.close()
     os.chdir(curdir)
 
+    print "Sequential Flaky Tests"
     for test,r in output.items():
         if 'F' in r:
-            print test,r
+            print " -",test,r
 
     statistics = compute_statistcs(output)
     assert statistics['skips'] == 0, "Should not have skipped tests here"
