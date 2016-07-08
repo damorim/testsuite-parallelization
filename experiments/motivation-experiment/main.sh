e49cf6e594c35c3ab56d14acb4a4113e008dd709
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-08 01:47:06 -0300

    Uncommented existing subjects

--- experiments/motivation-experiment/main.sh
+++ experiments/motivation-experiment/main.sh
@@ -17,7 +17,7 @@ fi
 # Possibly with deadlock when executed with parallel threads
 # ./experiment.sh "$SUBJECTS_HOME/graphhopper" "core" > graphhopper-summary.txt
 
-#./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
-#./experiment.sh "$SUBJECTS_HOME/camel/camel-core" > camel-core-summary.txt
-#./experiment.sh "$SUBJECTS_HOME/jetty.project" "jetty-client" > jetty.project-summary.txt
-#./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests" > okhttp-summary.txt
+./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
+./experiment.sh "$SUBJECTS_HOME/camel/camel-core" > camel-core-summary.txt
+./experiment.sh "$SUBJECTS_HOME/jetty.project" "jetty-client" > jetty.project-summary.txt
+./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests" > okhttp-summary.txt
