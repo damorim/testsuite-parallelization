42e122f0c865a5dc578fceb50e398b1eb8fd2479
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-10 00:57:18 -0300

    Updated main.sh

--- experiments/motivation-experiment/main.sh
+++ experiments/motivation-experiment/main.sh
@@ -18,7 +18,11 @@ time ./experiment.sh "$SUBJECTS_HOME/retrofit" "retrofit"
 # Possibly with deadlock when executed with parallel threads
 # ./experiment.sh "$SUBJECTS_HOME/graphhopper" "core" > graphhopper-summary.txt
 
-#./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
+#echo "Running experiment on \"JGit\""
+#time ./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test"
+
 #./experiment.sh "$SUBJECTS_HOME/camel/camel-core" > camel-core-summary.txt
 #./experiment.sh "$SUBJECTS_HOME/jetty.project" "jetty-client" > jetty.project-summary.txt
-#./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests" > okhttp-summary.txt
+
+#echo "Running experiment on \"OkHttp\""
+#time ./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests"
