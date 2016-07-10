533483b640fac3dc528f52e5c8e3e1f1ff4c111a
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-09 22:49:28 -0300

    Measuring experiment time

--- experiments/motivation-experiment/main.sh
+++ experiments/motivation-experiment/main.sh
@@ -12,12 +12,13 @@ if [ ! -d "$SUBJECTS_HOME" ]; then
     exit 1
 fi
 
-./experiment.sh "$SUBJECTS_HOME/retrofit" "retrofit" > retrofit-summary.txt
+echo "Running experiment on \"Retrofit\""
+time ./experiment.sh "$SUBJECTS_HOME/retrofit" "retrofit"
 
 # Possibly with deadlock when executed with parallel threads
 # ./experiment.sh "$SUBJECTS_HOME/graphhopper" "core" > graphhopper-summary.txt
 
-./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
-./experiment.sh "$SUBJECTS_HOME/camel/camel-core" > camel-core-summary.txt
-./experiment.sh "$SUBJECTS_HOME/jetty.project" "jetty-client" > jetty.project-summary.txt
-./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests" > okhttp-summary.txt
+#./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test" > jgit-summary.txt
+#./experiment.sh "$SUBJECTS_HOME/camel/camel-core" > camel-core-summary.txt
+#./experiment.sh "$SUBJECTS_HOME/jetty.project" "jetty-client" > jetty.project-summary.txt
+#./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests" > okhttp-summary.txt
