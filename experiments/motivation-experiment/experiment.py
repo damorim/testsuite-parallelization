85519d6171159b9f44d382b04ae8bba70c578bda
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-18 02:19:22 -0300

    Updated experiment scripts

---
+++ experiments/motivation-experiment/experiment.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python2
+import os
+
+import xml.etree.ElementTree as et
+
+from datetime import datetime
+from time import time
+
+from subprocess import call
+from shutil import rmtree
+from sys import argv
+from os import path
+
+class TestReport:
+    def __init__(self):
+        self.success = set({})
+        self.fail = set({})
+        self.ignored = set({})
+        self.flaky = set({})
+        self.total = 0
+
+    def __repr__(self):
+        repr = "Total: {0}, Ignored: {1}, Runs: {2}, Success: {3}, Flaky: {4}, Fail: {5}"
+        return repr.format(self.total, len(self.ignored), self.total - len(self.ignored), \
+                           len(self.success), len(self.flaky), len(self.fail))
+
+    def addpass(self, tc):
+        self.total += 1
+        self.success.add(tc)
+        assert self.total == (len(self.success) + len(self.fail) \
+                              + len(self.ignored) + len(self.flaky))
+
+    def addskip(self, tc):
+        self.total += 1
+        self.ignored.add(tc)
+        assert self.total == (len(self.success) + len(self.fail) \
+                              + len(self.ignored) + len(self.flaky))
+
+    def addfail(self, tc):
+        self.total += 1
+        self.fail.add(tc)
+        assert self.total == (len(self.success) + len(self.fail) \
+                              + len(self.ignored) + len(self.flaky))
+    def addflaky(self, tc):
+        self.total += 1
+        self.flaky.add(tc)
+        assert self.total == (len(self.success) + len(self.fail) \
+                              + len(self.ignored) + len(self.flaky))
+
+
+def collect_test_results(directory):
+    report = TestReport()
+    xmlfiles = [ f for f in os.listdir(directory) if f.startswith('TEST') and f.endswith('.xml') ]
+    hasChild = lambda n,t: n.find(t) is not None
+    for xmlfile in xmlfiles:
+        root = et.parse(os.path.join(reports_dir, xmlfile)).getroot()
+        for tc in root.findall('testcase'):
+            tcname = "{0}#{1}".format(tc.get('classname'), tc.get('name'))
+            if not len(list(tc)):
+                report.addpass(tcname)
+            elif hasChild(tc, 'skipped'):
+                report.addskip(tcname)
+            elif hasChild(tc, 'flakyError') or hasChild(tc, 'flakyFailure'):
+                report.addflaky(tcname)
+            else:
+                report.addfail(tcname)
+    return report
+
+
+def system_call(cmds, logpath=os.devnull):
+    with open(logpath, 'a') as log:
+        starttime = time()
+        call(cmds, stdout=log, stderr=log)
+        elapsedtime = time() - starttime
+        return elapsedtime
+
+
+# Input parameters
+version = argv[1]
+project_path = argv[2]
+test_rel_path = argv[3]
+
+# Constants
+base_dir = path.abspath(os.curdir)
+project_root = path.abspath(project_path)
+test_path = path.join(project_root, test_rel_path)
+project_name = path.basename(project_root)
+
+timestamp = datetime.fromtimestamp(time()).strftime("%m%d%H%M")
+log_prefix = "{0}-{1}-{2}".format(project_name, versioni, timestamp)
+
+print_msg = "Setup:\nVersion: {0}\nProject Path: {1}\nTest Path:{2}"
+print print_msg.format(version, project_root, test_path)
+
+# Checking elapsed time
+os.chdir(test_path)
+elapsedtime = system_call(['mvn', '-Dmaven.javadoc.skip', 'test'])
+print "Test Elapsed time:", elapsedtime
+
+# Logging time
+log_file_path = os.path.join(base_dir, log_prefix + "time.txt")
+with open(log_file_path, "a") as timelog:
+    timelog.write(str(elapsedtime) + "\n")
+
+# FLAKINESS EXPERIMENT
+BOUND = 5
+reports_dir = path.join(test_path, 'target', 'surefire-reports')
+
+# Maven rerun
+print "\nMaven RERUN [BOUND: {0}]".format(BOUND)
+rmtree(reports_dir)
+log_file_path = os.path.join(base_dir, log_prefix + "flakiness-mvnrerun.txt")
+elapsedtime = system_call(['mvn', '-Dsurefire.rerunFailingTestsCount=' + str(BOUND),
+                           '-Dmaven.javadoc.skip','test'], log_file_path)
+
+print "Elapsed time:", elapsedtime
+report = collect_test_results(reports_dir)
+print report
+
+# Iterative rerun
+print "\nIterative rerun [BOUND: {0}]".format(BOUND)
+rmtree(reports_dir)
+log_file_path = os.path.join(base_dir, log_prefix + "flakiness-rerun.txt")
+elapsedtime = 0
+failures = set({})
+for rep in range(BOUND):
+    elapsedtime =+ system_call(['mvn','-Dmaven.javadoc.skip','test'], log_file_path)
+    report = collect_test_results(reports_dir)
+    print "#{0}:".format(rep + 1), report
+    failures = failures.union(report.fail)
+
+print "Total Fails:", len(failures)
+
+individual_fails = set({})
+for tc in failures:
+    for i in range(BOUND):
+        rmtree(reports_dir)
+        elapsedtime += system_call(['mvn', '-Dmaven.javadoc.skip', '-Dtest=' + tc,
+                                    'test'], log_file_path)
+        report = collect_test_results(reports_dir)
+        if len(report.fail):
+            individual_fails.add(tc)
+            break
+
+print "Elapsed time:", elapsedtime
+print "Total:", len(failures), "Success:", len(failures) - len(individual_fails), \
+      "Fail:", len(individual_fails)
+
+# Exhaustive execution
+print "\nExhaustive execution [THRESHOLD: {0}]".format(BOUND)
+rmtree(reports_dir)
+log_file_path = os.path.join(base_dir, log_prefix + "flakiness-exhaustive.txt")
+elapsedtime = 0
+failures = set({})
+
+runs = 0
+i = 1
+while runs < BOUND:
+    elapsedtime =+ system_call(['mvn','-Dmaven.javadoc.skip','test'], log_file_path)
+
+    exhausted = True
+    report = collect_test_results(reports_dir)
+    old_size = len(failures)
+    failures = failures.union(report.fail)
+    if len(failures) > old_size:
+        exhausted = False
+
+    print "#{0}:".format(i), exhausted, report
+
+    if exhausted:
+        runs += 1
+    else:
+        runs = 0
+
+    i += 1
+
+print "Total Fails:", len(failures)
+
+individual_fails = set({})
+for tc in failures:
+    for i in range(BOUND):
+        rmtree(reports_dir)
+        elapsedtime += system_call(['mvn', '-Dmaven.javadoc.skip', '-Dtest=' + tc,
+                                    'test'], log_file_path)
+        report = collect_test_results(reports_dir)
+        if len(report.fail):
+            individual_fails.add(tc)
+            break
+
+print "Elapsed time:", elapsedtime
+print "Total:", len(failures), "Success:", len(failures) - len(individual_fails), \
+      "Fail:", len(individual_fails)
--- experiments/motivation-experiment/experiment.sh
+++ experiments/motivation-experiment/experiment.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 BASE_DIR="`pwd`"
+BUILD_FILES_DIR="$BASE_DIR/buildfiles"
+
+replace_build_file() {
+    local target_dir=$1
+    local file_path=$2
+    local file_name="`basename $file_path`"
+
+    cp $file_path $target_dir
+    rm $target_dir/pom.xml
+    mv $target_dir/$file_name $target_dir/pom.xml
+}
 
 # Input parameters
 PROJECT_PATH=$1
-TEST_PATH=$2
+TEST_REL_PATH=$2
 
-# Assuming the relative path to test directory as "PROJECT_PATH"
-[[ -z "$TEST_PATH" ]] && TEST_PATH="."
-
-if [[ -z "$PROJECT_PATH" ]]; then
-    echo "Usage: ./experiment.sh <PROJECT_PATH> [<TEST_PATH>]"
+if [ -z "$PROJECT_PATH" ]; then
+    echo "Usage: ./experiment.sh <project path> [<test relative path>]"
     exit 1
 fi
 
-NAME="`basename "$PROJECT_PATH"`"
-SAMPLE_HOME="$BASE_DIR/samples/$NAME"
+# Assuming the relative path to test directory as "PROJECT_PATH"
+[[ -z "$TEST_REL_PATH" ]] && TEST_REL_PATH="."
 
-# Removing old files before generating new project
-rm -rf "$SAMPLE_HOME"
-../scripts/generate_versions.sh "$PROJECT_PATH" "$TEST_PATH"
+NAME="`basename "$PROJECT_PATH"`"
+BUILD_FILES_HOME="$BUILD_FILES_DIR/$NAME"
+
+cd "$PROJECT_PATH"
+mvn clean install -DskipTest -Dmaven.javadoc.skip &>/dev/null
+cd "$BASE_DIR"
 
 echo "Running Experiment on subject \"$NAME\""
-for version in `ls $SAMPLE_HOME`; do
-    OUTPUT_FILE="$BASE_DIR/$NAME-$version-summary.txt"
-    echo -e "\n\t* Running setup \"$version\"" >> $OUTPUT_FILE
-    echo -e "\t* Started: `date`\n" >> $OUTPUT_FILE
-    ./flakiness_experiment.py "$NAME" "$SAMPLE_HOME/$version" "$TEST_PATH" >> $OUTPUT_FILE
-    echo -e "\n\t* Finished: `date`\n" >> $OUTPUT_FILE
-done
+for file in `ls $BUILD_FILES_HOME`; do
+    replace_build_file $PROJECT_PATH/$TEST_REL_PATH $BUILD_FILES_HOME/$file
 
-[[ ! -d outputs ]] && mkdir outputs
-mv *.txt outputs
+    # Should have no difference since they have to be the same file after
+    # replacing the original file
+    diff $BUILD_FILES_HOME/$file $PROJECT_PATH/$TEST_REL_PATH/pom.xml
+
+    VERSION="`basename ${file%.*xml}`"
+    OUTPUT_FILE="$BASE_DIR/$NAME-$VERSION-summary.txt"
+    {
+        ./experiment.py $VERSION $PROJECT_PATH $TEST_REL_PATH
+        echo "------------------"
+
+    } >> $OUTPUT_FILE
+
+done
