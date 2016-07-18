a93334a2394bf21a1b6d18da3b14a28c96456aa8
Jeanderson Candido <jeandersonbc@gmail.com>
2016-07-18 02:20:12 -0300

    Running all subjects

--- experiments/motivation-experiment/main.sh
+++ experiments/motivation-experiment/main.sh
@@ -5,19 +5,16 @@
 #
 # Author: Jeanderson Candido
 #
-SUBJECTS_HOME="../subjects"
+SUBJECTS_HOME="subjects"
 
-if [ ! -d "$SUBJECTS_HOME" ]; then
-    echo "Download subjects first before proceeding"
-    exit 1
-fi
+../scripts/download_subjects.sh
 
-#./experiment.sh "$SUBJECTS_HOME/retrofit" "retrofit"
-./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test"
-./experiment.sh "$SUBJECTS_HOME/graphhopper" "core"
-./experiment.sh "$SUBJECTS_HOME/jetty.project" "jetty-client"
-./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests"
-# FIXME: Nao compilou ./experiment.sh "$SUBJECTS_HOME/vert.x"
-# FIXME: Nao executou testes ./experiment.sh "$SUBJECTS_HOME/eclipse-collections" "unit-tests"
-./experiment.sh "$SUBJECTS_HOME/junit4"
-./experiment.sh "$SUBJECTS_HOME/camel/camel-core"
+./experiment.sh "$SUBJECTS_HOME/retrofit" "retrofit"
+#./experiment.sh "$SUBJECTS_HOME/jgit" "org.eclipse.jgit.test"
+#./experiment.sh "$SUBJECTS_HOME/graphhopper" "core"
+#./experiment.sh "$SUBJECTS_HOME/jetty.project" "jetty-client"
+#./experiment.sh "$SUBJECTS_HOME/okhttp" "okhttp-tests"
+#./experiment.sh "$SUBJECTS_HOME/vert.x"
+#./experiment.sh "$SUBJECTS_HOME/eclipse-collections" "unit-tests"
+#./experiment.sh "$SUBJECTS_HOME/junit4"
+#./experiment.sh "$SUBJECTS_HOME/camel/camel-core"
