efa07090938e1e4205577563a161b0516c45dc36
Jeanderson Candido <jeandersonbc@gmail.com>
2016-10-12 23:05:43 -0300

    Updated driver

--- experiments/test-regression-cost-eval/driver.py
+++ experiments/test-regression-cost-eval/driver.py
@@ -5,56 +5,91 @@
 import os
 import json
 import urllib.request
-from subprocess import call, Popen, PIPE
+from subprocess import call, check_output
 
+import helpers
 from ghwrappers.search import RepositoryQuery
 
 
-def run(queryable):
+RAW_DATA_DIR = os.path.abspath(os.curdir)
+
+# FIXME temporary flag to limit execution
+MAX_PROJECTS = 5
+
+def run(queryable, callback=None):
     # FIXME: Consider handling pagination
     with urllib.request.urlopen(queryable.query()) as response:
-        return json.loads(response.read().decode())
+        data = json.loads(response.read().decode())
+        print("Total items from criteria:", data["total_count"], "\n")
+        if callback:
+            callback(data)
 
 
-def run_tests_from(project):
-    working_dir = os.path.abspath(os.curdir)
-    os.chdir(project)
+def analyze(data):
+    column_sep = ", "
+    csv_file = os.path.join(RAW_DATA_DIR, "subjects-data.csv")
 
-    # FIXME: consider detecting build system instead of assuming Maven
-    p = Popen(["mvn", "clean", "test"], stderr=PIPE, stdout=PIPE)
-    stdout, stderr = p.communicate()
+    with open(csv_file, "w") as csv:
+        csv.write(column_sep.join(["SUBJECT", "URL", "VERSION", "BUILDER",
+                                   "COMPILED", "TESTED", "ELAPSED_TIME"]))
+        csv.write("\n")
 
-    print("Return code:", p.returncode)
-    if not p.returncode:
-        stdout = stdout.decode()
-        with open("stdout-log.txt", "w") as log:
-            log.write(stdout)
-    else:
-        stderr = stderr.decode()
-        with open("stderr-log.txt", "w") as log:
-            log.write(stderr)
+    item_counter = 1
+    for item in data["items"]:
+        if item_counter > MAX_PROJECTS:
+            break
 
-    os.chdir(working_dir)
+        proj_name = item["name"]
+        git_url = item["html_url"]
+
+        # Cloning if project doesn't exist
+        if not os.path.exists(proj_name):
+            call(["git", "clone", git_url])
+
+        # Moving to project directory to collect data and coming back
+        # to the working directory.
+        working_dir = os.path.abspath(os.curdir)
+        os.chdir(proj_name)
+
+        commit_ver_raw = check_output(["git", "rev-parse", "HEAD"])
+        commit_ver = commit_ver_raw.decode().strip()
+
+        print("{id}) {subj}".format(id=item_counter, subj=proj_name))
+        try:
+            builder = helpers.detect_build_system()
+            compiled = builder.compile()
+            tested = False if not compiled else builder.test()
+            elapsed_time = builder.test_elapsed_time()
+
+        except Exception as err:
+            print(err)
+            builder = "N/A"
+            compiled = "N/A"
+            tested = "N/A"
+            elapsed_time = "N/A"
+
+        os.chdir(working_dir)
+
+        log_info = []
+        log_info.append(proj_name)
+        log_info.append(git_url)
+        log_info.append(commit_ver)
+        log_info.append(str(builder))
+        log_info.append(str(compiled))
+        log_info.append(str(tested))
+        log_info.append(elapsed_time)
+
+        with open(csv_file, "a") as csv:
+            csv.write(column_sep.join(log_info))
+            csv.write("\n")
+
+        item_counter += 1
 
 
-if not os.path.exists("subjects"):
-    os.mkdir("subjects")
+if __name__ == "__main__":
+    if not os.path.exists("subjects"):
+        os.mkdir("subjects")
 
-os.chdir("subjects")
+    os.chdir("subjects")
 
-data = run(RepositoryQuery().lang("java").stars(">=100"))
-print("Total items from criteria:", data["total_count"])
-counter = 1
-for item in data["items"]:
-    proj_name = item["name"]
-    git_url = item["html_url"]
-
-    if not os.path.exists(proj_name):
-        call(["git", "clone", git_url])
-
-    print("{curr}/{total} - Trying to run tests from {name}".format(name=proj_name,
-                                                                    total=len(data["items"]),
-                                                                    curr=counter))
-    run_tests_from(proj_name)
-    counter += 1
-
+    run(RepositoryQuery().lang("java").stars(">=100"), analyze)
