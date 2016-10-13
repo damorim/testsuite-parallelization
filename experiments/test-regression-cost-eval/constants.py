91b8158af831f2a0f6d48dd2b162cd29ad4a2897
Jeanderson Candido <jeandersonbc@gmail.com>
2016-10-13 19:33:17 -0300

    Moved constants to a dedicated module

---
+++ experiments/test-regression-cost-eval/constants.py
@@ -0,0 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.curdir)
+RAW_DATA_DIR = BASE_DIR
+
+SUBJECT_DIR = os.path.join(os.path.abspath(os.curdir), "subjects")
+SUBJECTS_CSV_FILE = os.path.join(RAW_DATA_DIR, "subjects.csv")
+
+TIMECOST_CSV_FILE = os.path.join(RAW_DATA_DIR, "timecost.csv")
+COLUMN_SEP = ","
+
