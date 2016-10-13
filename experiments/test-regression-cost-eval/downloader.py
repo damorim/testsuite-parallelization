e03c9e4fd546eb6bc468ce242e7f705ffd16f5ae
Jeanderson Candido <jeandersonbc@gmail.com>
2016-10-13 19:34:37 -0300

    Moved constants and analysis code

--- experiments/test-regression-cost-eval/downloader.py
+++ experiments/test-regression-cost-eval/downloader.py
@@ -9,15 +9,8 @@ from subprocess import call, check_output
 from urllib.error import HTTPError
 
 import helpers
+from constants import SUBJECT_DIR, SUBJECTS_CSV_FILE
 from ghwrappers.search import RepositoryQuery
-
-BASE_DIR = os.path.abspath(os.curdir)
-RAW_DATA_DIR = BASE_DIR
-SUBJECT_DIR = os.path.join(os.path.abspath(os.curdir, "subjects")
-
-TIMECOST_CSV_FILE = os.path.join(RAW_DATA_DIR, "timecost.csv")
-SUBJECTS_CSV_FILE = os.path.join(RAW_DATA_DIR, "subjects.csv")
-COLUMN_SEP = ", "
 
 
 def run(queryable, callback=None):
@@ -45,36 +38,21 @@ def download(data):
         if not os.path.exists(proj_name):
             call(["git", "clone", "--depth", "1", git_url])
 
-        # Moving to project directory to collect data and coming back
-        # to the working directory.
-        working_dir = os.path.abspath(os.curdir)
+        # Moving to project directory to collect data and going back
+        # to the subject directory.
         os.chdir(proj_name)
 
         commit_ver_raw = check_output(["git", "rev-parse", "HEAD"])
         commit_ver = commit_ver_raw.decode().strip()
         builder = helpers.detect_build_system()
 
-# TODO: Lines commented because we want to build the subject
-#       list first with each respective commit version.
-#
-#            compiled = builder.compile()
-#            tested = False if not compiled else builder.test()
-#            elapsed_time = builder.test_elapsed_time()
-#
-#            compiled = "N/A"
-#            tested = "N/A"
-#            elapsed_time = "N/A"
-
-        os.chdir(working_dir)
+        os.chdir(SUBJECT_DIR)
 
         csv_line = []
         csv_line.append(proj_name)
         csv_line.append(git_url)
         csv_line.append(commit_ver)
         csv_line.append(str(builder))
-#        csv_line.append(str(compiled))
-#        csv_line.append(str(tested))
-#        csv_line.append(elapsed_time)
 
         with open(SUBJECTS_CSV_FILE, "a") as csv:
             csv.write(COLUMN_SEP.join(csv_line))
