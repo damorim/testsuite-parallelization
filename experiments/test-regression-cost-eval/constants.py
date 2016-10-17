77d9cf20cde79616b24f13ff2f8fc053b49c3e22
Jeanderson Candido <jeandersonbc@gmail.com>
2016-10-17 13:57:36 -0300

    Refactorings

--- experiments/test-regression-cost-eval/constants.py
+++ experiments/test-regression-cost-eval/constants.py
@@ -4,8 +4,9 @@ BASE_DIR = os.path.abspath(os.curdir)
 RAW_DATA_DIR = BASE_DIR
 
 SUBJECT_DIR = os.path.join(os.path.abspath(os.curdir), "subjects")
+SUBJECTS_CSV_HEADER_FIELDS = ["SUBJECT", "URL", "VERSION"]
 SUBJECTS_CSV_FILE = os.path.join(RAW_DATA_DIR, "subjects.csv")
 
 TIMECOST_CSV_FILE = os.path.join(RAW_DATA_DIR, "timecost.csv")
-COLUMN_SEP = ","
 
+COLUMN_SEP = ","
--- experiments/test-regression-cost-eval/downloader.py
+++ experiments/test-regression-cost-eval/downloader.py
@@ -2,70 +2,19 @@
 #
 # Author: Jeanderson Candido
 #
-import os
 import json
+import os
 import urllib.request
 from subprocess import call, check_output
 from urllib.error import HTTPError
 
-import helpers
-from constants import SUBJECT_DIR, SUBJECTS_CSV_FILE
+from constants import SUBJECT_DIR, SUBJECTS_CSV_FILE, COLUMN_SEP, SUBJECTS_CSV_HEADER_FIELDS
 from ghwrappers.search import RepositoryQuery
 
 
-def run(queryable, callback=None):
-    """
-    This routine executes the given query and send the output data
-    to a callback function (if any).
-    """
-    url = queryable.query()
-    print(url)
-    with urllib.request.urlopen(queryable.query()) as response:
-        data = json.loads(response.read().decode())
-        if callback:
-            callback(data)
-
-
-def download(data):
-    """
-    Callback function
-    """
-    for item in data["items"]:
-        proj_name = item["name"]
-        git_url = item["html_url"]
-
-        # Cloning if project doesn't exist
-        if not os.path.exists(proj_name):
-            call(["git", "clone", "--depth", "1", git_url])
-
-        # Moving to project directory to collect data and going back
-        # to the subject directory.
-        os.chdir(proj_name)
-
-        commit_ver_raw = check_output(["git", "rev-parse", "HEAD"])
-        commit_ver = commit_ver_raw.decode().strip()
-        builder = helpers.detect_build_system()
-
-        os.chdir(SUBJECT_DIR)
-
-        csv_line = []
-        csv_line.append(proj_name)
-        csv_line.append(git_url)
-        csv_line.append(commit_ver)
-        csv_line.append(str(builder))
-
-        with open(SUBJECTS_CSV_FILE, "a") as csv:
-            csv.write(COLUMN_SEP.join(csv_line))
-            csv.write("\n")
-
-
-if __name__ == "__main__":
-    if not os.path.exists(SUBJECT_DIR):
-        os.mkdir(SUBJECT_DIR)
-
-    os.chdir(SUBJECT_DIR)
+def download_from_github():
     with open(SUBJECTS_CSV_FILE, "w") as csv:
-        csv.write(COLUMN_SEP.join(["SUBJECT", "URL", "VERSION", "BUILDER"]))
+        csv.write(COLUMN_SEP.join(SUBJECTS_CSV_HEADER_FIELDS))
         csv.write("\n")
 
     max_pages = None
@@ -77,14 +26,50 @@ if __name__ == "__main__":
             if max_pages and current_page > max_pages:
                 break
             print("Processing page", current_page)
-            criteria = {"language": "java",
-                        "stars": ">=100"}
+            criteria = {"language": "java", "stars": ">=100"}
 
-            run(RepositoryQuery(criteria).at(current_page)
-                                         .size(page_size), download)
+            query_url = RepositoryQuery(criteria).at(current_page).size(page_size).query()
+            with urllib.request.urlopen(query_url) as response:
+                data = json.loads(response.read().decode())
+                for item in data["items"]:
+                    project_name = item["name"]
+                    git_url = item["html_url"]
+
+                    # Cloning if project doesn't exist
+                    if not os.path.exists(project_name):
+                        call(["git", "clone", "--depth", "1", git_url])
+
+                    # Moving to project directory to collect data and going back
+                    # to the subject directory.
+                    os.chdir(project_name)
+
+                    commit_ver_raw = check_output(["git", "rev-parse", "HEAD"])
+                    commit_ver = commit_ver_raw.decode().strip()
+
+                    os.chdir(SUBJECT_DIR)
+
+                    csv_line = [project_name, git_url, commit_ver]
+                    with open(SUBJECTS_CSV_FILE, "a") as csv:
+                        csv.write(COLUMN_SEP.join(csv_line))
+                        csv.write("\n")
 
     except HTTPError as err:
         print(err)
         print("Github API Query has exhausted")
         print("Total pages:", current_page)
 
+
+def download_from_file(file_path):
+    file_abs_path = os.path.abspath(file_path)
+    if not os.path.exists(file_abs_path):
+        print("Invalid path:", file_abs_path)
+        exit(1)
+    # TODO: Feature to be implemented
+
+
+if __name__ == "__main__":
+    if not os.path.exists(SUBJECT_DIR):
+        os.mkdir(SUBJECT_DIR)
+
+    os.chdir(SUBJECT_DIR)
+    download_from_github()
