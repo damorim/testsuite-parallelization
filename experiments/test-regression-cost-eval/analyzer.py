42c87bd61901b40d057226d2450dab45fa4bfc02
Jeanderson Candido <jeandersonbc@gmail.com>
2016-10-18 02:14:01 -0300

    Added support to collect test data

--- experiments/test-regression-cost-eval/analyzer.py
+++ experiments/test-regression-cost-eval/analyzer.py
@@ -1,32 +1,10 @@
 #!/usr/bin/python3
+import csv
 import os
 
 from support import builders
-from support.constants import COLUMN_SEP
+from support.constants import COLUMN_SEP, TIMECOST_CSV_FILE, SUBJECTS_CSV_FILE
 from support.constants import SUBJECT_DIR
-
-
-# def timed_exec(command, time_limit=None):
-#     time_commands = ["/usr/bin/time", "-v"]
-#     time_commands.extend(command)
-#
-#     p = Popen(time_commands, stdout=PIPE, stderr=PIPE)
-#     print("PID:", p.pid)
-#
-#     if not time_limit:
-#         stdout_raw, stderr_raw = p.communicate()
-#     else:
-#         try:
-#             stdout_raw, stderr_raw = p.communicate(timeout=time_limit)
-#         except TimeoutExpired:
-#             p.kill()
-#             stdout_raw, stderr_raw = p.communicate()
-#
-#     with open("compile-log", "w") as log:
-#         log.write(stdout_raw.decode())
-#         if stderr_raw:
-#             log.write("----SDTERR----\n")
-#             log.write(stderr_raw.decode())
 
 
 def inspect(subject):
@@ -35,7 +13,9 @@ def inspect(subject):
         return
     os.chdir(subject_dir)
 
-    data = {"builder_name": "N/A", "#tests": "N/A", "compiled": False, "tests_pass": False}
+    data = {"builder_name": "N/A", "compiled": False, "tests_pass": False, "#tests": "N/A",
+            "elapsed_t": "N/A", "system_t": "N/A", "user_t": "N/A", "cpu_usage": "N/A"}
+
     builder = builders.detect_system()
     if builder:
         data["builder_name"] = builder.name
@@ -45,47 +25,36 @@ def inspect(subject):
 
         # STEP 2: TEST SUBJECT AND COLLECT DATA
         data["tests_pass"] = builder.test(data)
-
-        # %e: Elapsed time (in seconds)
-        # %P: CPU usage
         # FIXME: How to deal when the project has multiple threads?
-        # time_args = ["%e", "%P"]
-        # test_command = ["time", "-f", COLUMN_SEP.join(time_args)]
-        # test_command = []
-        # test_command.extend(builder.test())
-        # pid = Popen(test_command, stderr=PIPE)
-        # out, err = pid.communicate()
-        # stream_lines = err.splitlines()
-        # print(stream_lines[len(stream_lines) - 1])
 
     return COLUMN_SEP.join([subject, data["builder_name"], str(data["compiled"]), str(data["tests_pass"]),
-                            data["#tests"]])
+                            str(data["#tests"]), data["elapsed_t"], data["system_t"], data["user_t"],
+                            data["cpu_usage"]])
 
-# FIXME
-# def main():
-#     # Limit execution
-#     max_rows = None
-#
-#     with open(TIMECOST_CSV_FILE, "w") as timecost:
-#         timecost.write(COLUMN_SEP.join(["SUBJECT", "BUILDER", "COMPILED",
-#                                         "TESTS_PASS", "ELAPSED_TIME"]))
-#         timecost.write("\n")
-#
-#     with open(SUBJECTS_CSV_FILE, newline="") as subjects:
-#         reader = csv.DictReader(subjects)
-#         cur_row = 1
-#         for row in reader:
-#             if max_rows and cur_row > max_rows:
-#                 break
-#             project = row["SUBJECT"]
-#             if not row["BUILDER"] == "N/A":
-#                 print("Checking", project)
-#                 # csv_line = check(project)
-#                 # with open(TIMECOST_CSV_FILE, "a") as timecost:
-#                 #     timecost.write(COLUMN_SEP.join(csv_line))
-#                 #     timecost.write("\n")
-#                 cur_row += 1
+
+def main():
+    # Limit execution
+    max_rows = 5
+
+    with open(TIMECOST_CSV_FILE, "w") as timecost:
+        timecost.write(COLUMN_SEP.join(["SUBJECT", "BUILDER", "COMPILED", "TESTS_PASS", "ELAPSED_T",
+                                        "SYSTEM_T", "USER_T", "CPU_USAGE"]))
+        timecost.write("\n")
+
+    with open(SUBJECTS_CSV_FILE, newline="") as subjects:
+        reader = csv.DictReader(subjects)
+        cur_row = 1
+        for row in reader:
+            if max_rows and cur_row > max_rows:
+                break
+            project = row["SUBJECT"]
+            print("Checking", project)
+            csv_line = inspect(project)
+            with open(TIMECOST_CSV_FILE, "a") as timecost:
+                timecost.write(csv_line)
+                timecost.write("\n")
+            cur_row += 1
 
 
 if __name__ == "__main__":
-    print(inspect("retrofit"))
+    main()
--- experiments/test-regression-cost-eval/support/builders.py
+++ experiments/test-regression-cost-eval/support/builders.py
@@ -3,9 +3,8 @@
 # Author: Jeanderson Candido
 #
 import os
-from subprocess import call, Popen, PIPE
-
 import re
+from subprocess import call, Popen, PIPE, TimeoutExpired
 
 
 class AbstractBuilder:
@@ -16,7 +15,12 @@ class AbstractBuilder:
 
     def compile(self):
         """ Returns True if finished successfully """
-        return not call(self.compile_args)
+        with open("/dev/null", "wb") as DEVNULL:
+            try:
+                result = not call(self.compile_args, stdout=DEVNULL, timeout=1800)
+            except TimeoutExpired:
+                result = False
+        return result
 
     def test(self):
         pass
@@ -30,12 +34,33 @@ class Maven(AbstractBuilder):
         self.test_args = ["mvn", "test", "-Dmaven.javadoc.skip=true"]
 
     def test(self, data=None):
-        p = Popen(self.test_args, stdout=PIPE)
+        args = ["time", "-o", "/tmp/timeinfo", "-f", "%e,%S,%U,%P"]
+        args.extend(self.test_args)
+        with open("/dev/null", "wb") as DEVNULL:
+            p = Popen(args, stdout=PIPE, stderr=DEVNULL)
+        print("PID (use pstree):", p.pid)
         out, err = p.communicate()
 
-        # Fixme collect # of tests
-        output = out.decode()
-        print(output)
+        output = out.decode().splitlines()
+        total = 0
+        for i in range(len(output)):
+            line = output[i]
+            if line.startswith("Results :"):
+                for j in range(len(output)):
+                    test_line = output[i + j]
+                    if "run" in test_line:
+                        total += int(re.sub(",.*", "", re.sub("Tests run: ", "", test_line)))
+                        break
+        if data:
+            data["#tests"] = total
+            with open("/tmp/timeinfo") as f:
+                for line in f:
+                    if not line.startswith("Command"):
+                        output = line.strip().split(",")
+                        data["elapsed_t"] = output[0]
+                        data["system_t"] = output[1]
+                        data["user_t"] = output[2]
+                        data["cpu_usage"] = output[3]
 
         return not p.returncode
 
