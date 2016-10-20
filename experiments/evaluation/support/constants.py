ad19581f41797ca05e94047561bbcc679a5f2343
Jeanderson Candido <jeandersonbc@gmail.com>
2016-10-20 11:04:46 -0300

    Updated constants

--- experiments/evaluation/support/constants.py
+++ experiments/evaluation/support/constants.py
@@ -1,12 +1,9 @@
 import os
 
 BASE_DIR = os.path.abspath(os.curdir)
-RAW_DATA_DIR = BASE_DIR
 
 SUBJECT_DIR = os.path.join(os.path.abspath(os.curdir), "subjects")
 SUBJECTS_CSV_HEADER_FIELDS = ["SUBJECT", "URL", "VERSION"]
-SUBJECTS_CSV_FILE = os.path.join(RAW_DATA_DIR, "subjects.csv")
-
-TIMECOST_CSV_FILE = os.path.join(RAW_DATA_DIR, "timecost.csv")
-
+SUBJECTS_CSV_FILE = os.path.join(BASE_DIR, "subjects.csv")
+TIMECOST_CSV_FILE = os.path.join(BASE_DIR, "rawdata.csv")
 COLUMN_SEP = ","
