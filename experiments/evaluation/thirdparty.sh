92f4644e0f8b200ed43a7ce21c77671383b00394
Jeanderson Candido <jeandersonbc@gmail.com>
2016-11-02 12:02:17 -0300

    Script for thirdparty packages

---
+++ experiments/evaluation/thirdparty.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Installs required third-party packages
+# Author: Jeanderson Candido
+BASE_DIR=$(pwd)
+THIRDPARTY_HOME=$BASE_DIR/thirdparty
+
+if [ ! -d "$THIRDPARTY_HOME" ]; then
+    mkdir "$THIRDPARTY_HOME"
+fi
+cd "$THIRDPARTY_HOME"
+
+wget http://ftp.unicamp.br/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+wget http://mirror.nbtelecom.com.br/apache//ant/binaries/apache-ant-1.9.7-bin.tar.gz
+
+# TODO INSTALL MAVEN/ANT/JAVA
