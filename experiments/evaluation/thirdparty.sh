fc3edfaa40524ccf54abf61e617a32033a830ff5
Jeanderson Candido <jeandersonbc@gmail.com>
2016-11-02 22:06:54 -0300

    Added links about sysstat

--- experiments/evaluation/thirdparty.sh
+++ experiments/evaluation/thirdparty.sh
@@ -14,4 +14,11 @@ cd "$THIRDPARTY_HOME"
 wget http://ftp.unicamp.br/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 wget http://mirror.nbtelecom.com.br/apache//ant/binaries/apache-ant-1.9.7-bin.tar.gz
 
-# TODO INSTALL MAVEN/ANT/JAVA
+# TODO INSTALL MAVEN/ANT/JAVA/etc.... SYSSTAT
+#
+# About sysstat:
+#   https://www.blackmoreops.com/2014/06/18/sysstat-sar-examples-usage/
+#   http://www.thegeekstuff.com/2014/11/pidstat-examples/
+#   http://www.cyberciti.biz/tips/identifying-linux-bottlenecks-sar-graphs-with-ksar.html#more-6165
+#   http://www.thegeekstuff.com/2011/03/sar-examples/?utm_source=feedburner
+
