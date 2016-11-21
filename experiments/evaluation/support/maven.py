7222c0a0eb63d9ef0d2e7077534de2d9351d581f
Jeanderson Candido <jeandersonbc@gmail.com>
2016-11-21 12:41:27 -0300

    Fixes #9

--- experiments/evaluation/support/maven.py
+++ experiments/evaluation/support/maven.py
@@ -57,9 +57,6 @@ def collect_surefire_data():
     Collect data from surefire reports.
     Returns a Data tuple with statistics counter and a list of test cases data.
     """
-    if not has_surefire_dir():
-        raise Exception("Could not find surefire directory at \"{}\"".format(os.path.abspath(os.curdir)))
-
     output = check_output(["find", ".", "-name", "TEST-*.xml"]).decode()
     total_counter = Counter(tests=0, skipped=0, failure=0, time=0.0)
     test_cases = []
