a333b2744c136a4261c37fc00ec5dc8f4fa1a028
Jeanderson Candido <jeandersonbc@gmail.com>
2016-11-23 14:23:53 -0300

    Updated R script

--- experiments/evaluation/data/Main.R
+++ experiments/evaluation/data/Main.R
@@ -19,7 +19,7 @@ compute_groups <- function(elapsed_times) {
   return(groups)
 }
 
-rq1 <- function(rawdata, plot_path) {
+rq1_timecost <- function(rawdata, plot_path) {
   pdf(plot_path, height = 3.5, width = 5)
   
   # plot layout
@@ -33,21 +33,20 @@ rq1 <- function(rawdata, plot_path) {
   main_pies <- c("Sequential", "Standard")
   for (i in 1:2) {
     mode <- modes[i]
-    filtered_data <- rawdata[rawdata$mode == mode,]
-    groups <- compute_groups(filtered_data$elapsed_time)
+    df <- rawdata[rawdata$mode == mode, ]
+    groups <- compute_groups(df$elapsed_time)
     groups_distribution <- as.data.frame(table(groups))
     frequencies <- groups_distribution$Freq
     sum_freq <- sum(frequencies)
-    
-    print(paste("DEBUG: mode=", mode, " total=", sum_freq,
-                sep = ""))
+
+    print(paste("DEBUG: mode=", mode, " total=", sum_freq, sep = ""))
     print(groups_distribution)
-    pct <- round(frequencies / sum_freq * 100)
-    lbls <- paste(pct, "%", sep = "")
+
+    perct_values <- round(frequencies / sum_freq * 100)
     pie(
       frequencies,
       radius = 0.8,
-      labels = lbls,
+      labels = paste(perct_values, "%", sep = ""),
       col = c("gray30", "gray50", "gray90")
     )
     
@@ -65,7 +64,36 @@ rq1 <- function(rawdata, plot_path) {
   )
 }
 
+rq1_tests_time <- function(rawdata, plot_path) {
+  mode <- "L0"
+  column_filter <- c("name", "elapsed_time", "r_tests")
+  df <- rawdata[rawdata$mode == mode, column_filter]
+
+  # eliminates some unnecessary points
+  df <- df[df$r_tests <= 1000 & df$r_tests >= 100, ]
+
+  # Convert seconds to minutes
+  minutes <- double()
+  for (t in df$elapsed_time) {
+    minutes <- c(minutes, (t / 60))
+  }
+  print("DEBUG:")
+  print(df[order(df[, 2]),])
+
+  pdf(plot_path)
+  plot(
+    x = minutes,
+    y = df$r_tests,
+    xlab = "Elapsed time (min)",
+    ylab = "# of Tests",
+    pch = 16 # changes dot type
+  )
+}
+
 name <- "rawdata.csv"
 rawdata <- read.csv(name)
 
-rq1(rawdata, "plots/piechart-timecost.pdf")
+rq1_timecost(rawdata, "plots/piechart-timecost.pdf")
+rq1_tests_time(rawdata, "plots/scatter-tests-time.pdf")
+
+print(paste("Experiment cost:", sum(rawdata$elapsed_time), "s"))
