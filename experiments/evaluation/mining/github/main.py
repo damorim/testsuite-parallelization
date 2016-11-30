b0bb8e13342e3013bb1262060ab21b0775786221
Jeanderson Candido <jeandersonbc@gmail.com>
2016-11-30 10:21:50 -0300

    ordering by number of stars

--- experiments/evaluation/mining/github/main.py
+++ experiments/evaluation/mining/github/main.py
@@ -11,7 +11,12 @@ def generate_subject_list(subject_csv):
     with open(subject_csv, "w") as f:
         f.write("name,url\n")
 
-    criteria = {"language": "java", "stars": ">=100", "pushed": ">=2016-01-01", "mvn%20in": "readme"}
+    criteria = {"language": "java",
+                "stars": ">=100",
+                "pushed": ">=2016-01-01",
+                "mvn%20in": "readme",
+                "sort": "stars"}
+
     current_page = 0
     page_size = 100
     query_url = None
@@ -19,6 +24,7 @@ def generate_subject_list(subject_csv):
         while True:
             current_page += 1
             query_url = RepositoryQuery(criteria).at(current_page).size(page_size).query()
+            print("Fetching URL: {}".format(query_url))
             with urllib.request.urlopen(query_url) as response:
                 data = json.loads(response.read().decode())
                 for item in data["items"]:
