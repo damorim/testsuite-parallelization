c66ecfaeffa5e5aec9b2744e0f2926ac91063bf9
Jeanderson Candido <jeandersonbc@gmail.com>
2016-12-01 11:39:39 -0300

    Updated vagrant environment

--- experiments/evaluation/thirdparty.sh
+++ experiments/evaluation/thirdparty.sh
@@ -3,22 +3,24 @@
 # Installs required third-party packages for vagrant provisioning.
 #
 # Author: Jeanderson Candido
-BASE_DIR=$(pwd)
-THIRDPARTY_HOME=$BASE_DIR/thirdparty
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
 
-if [ ! -d "$THIRDPARTY_HOME" ]; then
-    mkdir "$THIRDPARTY_HOME"
+# INSTALLING Python Packages
+sudo apt-get install -y python3-lxml
+
+# INSTALLING GIT
+sudo apt-get install -y git
+
+# INSTALLING ORACLE JDK 8
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+# INSTALLING MAVEN
+if [ ! -d apache-maven-3.3.9 ]; then
+    wget http://ftp.unicamp.br/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+    tar zxvf apache-maven-3.3.9-bin.tar.gz
 fi
-cd "$THIRDPARTY_HOME"
 
-wget http://ftp.unicamp.br/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-wget http://mirror.nbtelecom.com.br/apache//ant/binaries/apache-ant-1.9.7-bin.tar.gz
-
-# TODO INSTALL MAVEN/ANT/JAVA/etc.... SYSSTAT
-#
-# About sysstat:
-#   https://www.blackmoreops.com/2014/06/18/sysstat-sar-examples-usage/
-#   http://www.thegeekstuff.com/2014/11/pidstat-examples/
-#   http://www.cyberciti.biz/tips/identifying-linux-bottlenecks-sar-graphs-with-ksar.html#more-6165
-#   http://www.thegeekstuff.com/2011/03/sar-examples/?utm_source=feedburner
-
+echo "export MAVEN_HOME=/home/vagrant/apache-maven-3.3.9" >> /home/vagrant/.profile
+echo "export PATH=\$MAVEN_HOME/bin:\$PATH" >> /home/vagrant/.profile
