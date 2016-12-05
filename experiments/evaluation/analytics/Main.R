ea0b0927ca6dbf2f73028a68b44181ae869149fb
Jeanderson Candido <jeandersonbc@gmail.com>
2016-12-05 14:04:56 -0300

    Updated R script

--- experiments/evaluation/analytics/Main.R
+++ experiments/evaluation/analytics/Main.R
@@ -20,6 +20,7 @@ rq1_timecost <- function(rawdata, plot_path) {
   )
   modes <- c("L0", "Standard")
   main_pies <- c("Sequential", "Standard")
+  cols <- c("gray30", "gray50", "gray90")
   for (i in 1:2) {
     mode <- modes[i]
     df <- rawdata[rawdata$mode == mode, ]
@@ -38,7 +39,7 @@ rq1_timecost <- function(rawdata, plot_path) {
       labels = sapply(percents, function(v) {
         return(paste(v, "%", sep = ""))
       }),
-      col = c("gray30", "gray50", "gray90")
+      col = cols
     )
     # bring pie title down
     title(main_pies[i], line = -6)
@@ -48,8 +49,8 @@ rq1_timecost <- function(rawdata, plot_path) {
   legend(
     "topleft",
     inset = c(0, 0.35),
-    c("short", "medium", "long"),
-    fill = c("gray30", "gray50", "gray90")
+    c("long", "medium", "short"),
+    fill = cols
   )
 }
 
@@ -72,13 +73,16 @@ rq1_tests_time <- function(rawdata, plot_path) {
 }
 
 rq2_prevalence <- function(df, output) {
-
+  print(df)
 }
 
 args <- commandArgs(trailingOnly = TRUE)
 rawdata <- read.csv(args[1])
 parprev <- read.csv(args[2])
 output <- args[3]
+# rawdata <- read.csv("../results/dataset-execution-1612031032.csv")
+# parprev <- read.csv("../results/dataset-parprev-1612031032.csv")
+# output <- "plots"
 
 rawdata$minutes <- sapply(rawdata$elapsed_time, function(v) {
   return(v / 60)
