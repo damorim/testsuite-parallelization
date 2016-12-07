a8cf145ec192acc8ffd69f1bb434a5c1eaef8a51
Jeanderson Candido <jeandersonbc@gmail.com>
2016-12-07 20:40:54 -0300

    Updated helper and python script

--- experiments/evaluation/analytics/Helper.R
+++ experiments/evaluation/analytics/Helper.R
@@ -7,14 +7,63 @@ which_group <- function(time) {
   }
   return("long")
 }
+
+piechart_timecost <- function(rawdata) {
+  # plot layout
+  par(
+    mfrow = c(1, 2),
+    mar = c(1, 1, 1, 1),
+    oma = c(0, 0, 0, 0),
+    xpd = T
+  )
+  cols <- c("gray30", "gray50", "gray90")
+
+  df <- rawdata[rawdata$mode == "Standard", ]
+  groups_distribution <- as.data.frame(table(df$group))
+  frequencies <- groups_distribution$Freq
+  sum_freq <- sum(frequencies)
+
+  percents = sapply(frequencies, function(v) {
+    return(round(v / sum_freq * 100, 1))
+  })
+  pie(
+    frequencies,
+    radius = 0.8,
+    main = "Distribution of execution cost",
+    labels = sapply(percents, function(v) {
+      return(paste(v, "%", sep = ""))
+    }),
+    col = cols
+  )
+  # bring pie title down
+  # title(main_pies[i], line = -6)
+
+  # plot legend
+  plot.new()
+  legend(
+    "topleft",
+    inset = c(0, 0.35),
+    c("long", "medium", "short"),
+    fill = cols
+  )
+}
+
 file_input <- "../results/dataset-execution-1612061818.csv"
 
 ds <- read.csv(file_input)
 ds <- ds[ds$mode == "Standard",]
+ds$minutes <- sapply(ds$elapsed_time, function(v) {
+  return(v / 60)
+})
 
 # Classify subjects according elapsed time
 ds$group <- sapply(ds$elapsed_time, FUN = which_group)
 table(ds$group)
+
+piechart_timecost(ds)
+temp <- ds[ds$group == "long" & ds$minutes <= 60,]
+boxplot(temp$minutes ~ temp$group, data = temp)
+
 
 # Total cost for Standard execution
 print(paste("[ALL]    Total cost in std mode (hours):", round(sum(ds[, "elapsed_time"]) / 3600, 2)))
--- experiments/evaluation/py/parallel-settings.py
+++ experiments/evaluation/py/parallel-settings.py
@@ -7,11 +7,14 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument("input")
 parser.add_argument("subjects")
-parser.add_argument("threshold", help="limits the subjects according to the elapsed time", type=float)
+parser.add_argument("--min", help="limits the subjects according to the elapsed time", type=float)
+parser.add_argument("--max", help="limits the subjects according to the elapsed time", type=float)
 args = parser.parse_args()
 
 subjects_home = os.path.abspath(args.subjects)
 base_dir = os.path.abspath(os.curdir)
+min_limit = 0 if not args.min else args.min
+max_limit = args.max
 
 
 def inspect(paths):
@@ -25,7 +28,7 @@ def inspect(paths):
 with open(args.input, newline="") as f:
     reader = csv.DictReader(f)
     for row in reader:
-        if float(row["elapsed_time"]) > args.threshold:
+        if float(row["elapsed_time"]) > min_limit and float():
             os.chdir(os.path.join(subjects_home, row["name"]))
             xml_paths = subprocess.check_output(["find", ".", "-path", "*/pom.xml"]).decode().splitlines()
             inspect(xml_paths)
