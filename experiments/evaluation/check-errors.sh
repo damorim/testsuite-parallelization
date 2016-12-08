fcd581ce9dfd6d930fd954f4efe37e5984e05d9b
Jeanderson Candido <jeandersonbc@gmail.com>
2016-12-08 18:38:43 -0300

    Updated rawdata

--- experiments/evaluation/check-errors.sh
+++ experiments/evaluation/check-errors.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "Not a maven project: `cat $1 | grep 'not a Maven project' | wc -l`"
-echo "Couldn't solve dependencies: `cat $1 | grep 'dependency:go-offline' | wc -l`"
-echo "Couldn't compile: `cat $1 | grep "install" | wc -l`"
-echo "Couldn't find reports: `cat $1 | grep "Couldn't find" | wc -l`"
-echo "Aborts detected: `cat $1 | grep "diverge" | wc -l`"
-echo "Total: `cat $1 | wc -l`"
+ERROR_LOG=experiment-errors.csv
+
+echo "Not a maven project: `cat $ERROR_LOG | grep 'not a Maven project' | wc -l`"
+echo "Couldn't solve dependencies: `cat $ERROR_LOG | grep 'dependency:go-offline' | wc -l`"
+echo "Couldn't compile: `cat $ERROR_LOG | grep "install" | wc -l`"
+echo "Couldn't find reports: `cat $ERROR_LOG | grep "Couldn't find" | wc -l`"
+echo "Aborts detected: `cat $ERROR_LOG | grep "diverge" | wc -l`"
+echo "Total: `cat $ERROR_LOG | wc -l`"
