91ace7dbfd2f49a1c2dea976f0cd8a48b9c1d783
Jeanderson Candido <jeandersonbc@gmail.com>
2017-01-17 16:48:05 -0300

    ignoring outliers

--- paper/R/testcost.r
+++ paper/R/testcost.r
@@ -3,7 +3,7 @@ library(ggplot2)
 pdf(file = "testcost-long.pdf", width = 5, height = 2)
 dat <- read.csv("testcost-long.csv", header = F) 
 p <- ggplot(dat[dat$V2<10,], aes(x=factor(V1), y=V2))
-p + geom_boxplot() +
+p + geom_boxplot(outlier.shape=NA) +
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
       axis.ticks.x=element_blank()
@@ -15,11 +15,12 @@ aux <- dev.off()
 pdf(file = "testcost-med.pdf", width = 5, height = 2)
 dat <- read.csv("testcost-med.csv", header = F) 
 p <- ggplot(dat[dat$V2<10,], aes(x=factor(V1), y=V2))
-p + geom_boxplot() +
+p + geom_boxplot(outlier.shape=NA) +
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
       axis.ticks.x=element_blank()
 ) +
 labs(y="Time (s)")
 
+p <- ggplot(dat, aes(x=factor(V1), y=V2), outlier.shape=NA)
 aux <- dev.off()
