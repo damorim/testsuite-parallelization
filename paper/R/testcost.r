ec3d806e82bdfb669087339bca227abc29709f30
Jeanderson Candido <jeandersonbc@gmail.com>
2017-01-17 16:34:03 -0300

    limited outliers up to 10 secs

--- paper/R/testcost.r
+++ paper/R/testcost.r
@@ -2,7 +2,7 @@ library(ggplot2)
 
 pdf(file = "testcost-long.pdf", width = 5, height = 2)
 dat <- read.csv("testcost-long.csv", header = F) 
-p <- ggplot(dat, aes(x=factor(V1), y=V2))
+p <- ggplot(dat[dat$V2<10,], aes(x=factor(V1), y=V2))
 p + geom_boxplot() +
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
@@ -14,7 +14,7 @@ aux <- dev.off()
 
 pdf(file = "testcost-med.pdf", width = 5, height = 2)
 dat <- read.csv("testcost-med.csv", header = F) 
-p <- ggplot(dat, aes(x=factor(V1), y=V2))
+p <- ggplot(dat[dat$V2<10,], aes(x=factor(V1), y=V2))
 p + geom_boxplot() +
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
