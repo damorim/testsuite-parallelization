ffdf16dd8936306e45d17517b29613367292a8c8
Jeanderson Candido <jeandersonbc@gmail.com>
2017-01-17 17:16:00 -0300

    work in progress - plots RQ2

--- paper/R/testcost.r
+++ paper/R/testcost.r
@@ -2,25 +2,27 @@ library(ggplot2)
 
 pdf(file = "testcost-long.pdf", width = 5, height = 2)
 dat <- read.csv("testcost-long.csv", header = F) 
-p <- ggplot(dat[dat$V2<10,], aes(x=factor(V1), y=V2))
+p <- ggplot(dat[dat$V2>1,], aes(x=factor(V1), y=V2))
 p + geom_boxplot(outlier.shape=NA) +
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
       axis.ticks.x=element_blank()
 ) +
-labs(y="Time (s)")
+labs(y="Time (s)")+
+coord_cartesian(ylim = c(1,150))
 
 aux <- dev.off()
 
 pdf(file = "testcost-med.pdf", width = 5, height = 2)
 dat <- read.csv("testcost-med.csv", header = F) 
-p <- ggplot(dat[dat$V2<10,], aes(x=factor(V1), y=V2))
+p <- ggplot(dat[dat$V2>1,], aes(x=factor(V1), y=V2))
 p + geom_boxplot(outlier.shape=NA) +
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
       axis.ticks.x=element_blank()
 ) +
-labs(y="Time (s)")
+labs(y="Time (s)") +
+coord_cartesian(ylim = c(1,90))
 
 p <- ggplot(dat, aes(x=factor(V1), y=V2), outlier.shape=NA)
 aux <- dev.off()
