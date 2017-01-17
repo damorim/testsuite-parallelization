70a62ea77e70ac648b095d749a9016394dc4ea96
Jeanderson Candido <jeandersonbc@gmail.com>
2017-01-17 08:34:57 -0300

    Updated plot with real data

--- paper/R/testcost.r
+++ paper/R/testcost.r
@@ -2,9 +2,9 @@ library(ggplot2)
 
 pdf(file = "testcost.pdf", width = 5, height = 2.5)
 
-dat <- read.csv("testcost.csv", header = T) 
+dat <- read.csv("testcost.csv", header = F) 
 
-p <- ggplot(dat, aes(x=factor(project), y=time))
+p <- ggplot(dat, aes(x=factor(V1), y=V2))
 
 p + geom_boxplot() +
 theme(axis.title.x=element_blank(),
