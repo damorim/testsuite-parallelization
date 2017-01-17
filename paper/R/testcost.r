9b9a9a727646c953084cf780fff621dea8e8b290
Jeanderson Candido <jeandersonbc@gmail.com>
2017-01-17 13:54:53 -0300

    Updated plots from RQ2

--- paper/R/testcost.r
+++ paper/R/testcost.r
@@ -1,11 +1,20 @@
 library(ggplot2)
 
-pdf(file = "testcost.pdf", width = 5, height = 2.5)
-
-dat <- read.csv("testcost.csv", header = F) 
-
+pdf(file = "testcost-long.pdf", width = 5, height = 2)
+dat <- read.csv("testcost-long.csv", header = F) 
 p <- ggplot(dat, aes(x=factor(V1), y=V2))
+p + geom_boxplot() +
+theme(axis.title.x=element_blank(),
+      axis.text.x=element_blank(),
+      axis.ticks.x=element_blank()
+) +
+labs(y="Time (s)")
 
+aux <- dev.off()
+
+pdf(file = "testcost-med.pdf", width = 5, height = 2)
+dat <- read.csv("testcost-med.csv", header = F) 
+p <- ggplot(dat, aes(x=factor(V1), y=V2))
 p + geom_boxplot() +
 theme(axis.title.x=element_blank(),
       axis.text.x=element_blank(),
