858b066ab966f384c3e8de021e7e2466bd07e629
damorim <damorim.marcelo@gmail.com>
2017-01-18 12:46:53 -0300

    pass on RQ2

--- paper/R/testcost.r
+++ paper/R/testcost.r
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(ggplot2)
+library(raster)
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -10,14 +11,21 @@ if (length(args)==0) {
   name = args[1]
 }
 
-pdf(file=paste(name,".pdf",sep=""), width = 5, height = 2)
+# ## coefficient of variation function
+# co.var <- function(x,na.rm=TRUE) sd(x,na.rm=na.rm)/mean(x,na.rm=na.rm)
+
+pdf(file=paste(name,".pdf",sep=""), width = 5, height = 1.5)
 
 dat <- read.csv(paste(name,".csv",sep=""), header = F)
 
-## to compute mean across plots
-datasel <- aggregate(dat[, 2:2], list(dat$V1), mean)
+## to compute median values across plots
+median <- aggregate(dat[, 2:2], list(dat$V1), median)
 
-p <- ggplot(dat[dat$V2>1,], aes(x=factor(V1), y=V2))
+print("==sd==>")
+sd <- aggregate(dat[, 2:2], list(dat$V1), sd)
+print(sd)
+
+p <- ggplot(dat, aes(x=factor(V1), y=V2))
 
 p + geom_boxplot(outlier.shape=NA) +
 theme(axis.title.x=element_blank(),
@@ -25,8 +33,8 @@ theme(axis.title.x=element_blank(),
       axis.ticks.x=element_blank()
 ) +
 labs(y="Time (s)")+
-geom_hline(aes(yintercept=mean(datasel$x, linetype = 2, colour = "red")))+
-coord_cartesian(ylim = c(1,25))
+geom_hline(aes(yintercept=mean(median$x)), colour = "red", linetype = 2)+
+coord_cartesian(ylim = c(0,7.5))
 
 aux <- dev.off()
 
