c1bde87543f559e757dfb693dea61ffe081d8181
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2017-05-11 21:22:57 -0300

    pass on scalability

--- paper/R/scalability/scalability.r
+++ paper/R/scalability/scalability.r
@@ -1,7 +1,7 @@
-pdf(file = "scalability.pdf", width = 5, height = 3)
+pdf(file = "scalability.pdf", width = 7, height = 4)
 
 dataframe <- read.csv(file="scalability.data", header=TRUE, sep=",")
 
-boxplot(time~numcores, data=dataframe, ylab="Time (m)", xlab="# Cores", ylim=c(0,max(dataframe$time)))
+boxplot(time~numcores, data=dataframe, ylab="time (m)", xlab="# cores", ylim=c(0,max(dataframe$time)), cex.lab=1.8, cex.axis=1.8)
 
 aux <- dev.off()
