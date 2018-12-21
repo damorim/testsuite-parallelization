0414f4b0947e78f411d7162c91d13248c79721bc
Marcelo D'Amorim <marcelodamorim@MacBook-Pro-de-Marcelo.local>
2018-12-21 07:20:20 -0300

    better logging

--- scripts/find-compiled.py
+++ scripts/find-compiled.py
@@ -8,7 +8,7 @@ from subprocess import call, check_output
 from utils import TimeInterval, CSVOutput, Query, verify_maven_support, Filter, setup_logger
 
 
-find_compiled_logger = setup_logger("find-compiled","find-compiled.log")
+logger = setup_logger("find-compiled","find-compiled.log")
 
 def git_revision(project_path):
     """
@@ -34,7 +34,7 @@ def main():
     if not os.path.exists(download_dir):
         os.mkdir(download_dir)
 
-    find_compiled_logger.info('starting...')
+    logger.info('starting...')
     # iterating through interesting.csv rows
     currentdir = os.getcwd()
     with open('interesting.csv') as csvfile:
@@ -44,34 +44,35 @@ def main():
             branch = row['default_branch']
             depth = 1
             dest = os.path.join(output_dir, full_name.replace(os.sep, "_"))
+            logger.warning("processing project {}".format(full_name))
             # clone only the head of the project into download directory
             git_cmd = "git clone http://github.com/{} -b {} --depth {} --recursive {}".format(full_name, branch, depth, dest)
             call(git_cmd, shell=True)
-            find_compiled_logger.warning("cloned project {}".format(full_name))
+            logger.warning("  cloned project")
             # clean project and download all dependencies
             os.chdir(dest)
             clean_command = "mvn clean dependency:go-offline"
             #TODO: check if we should interrupt if some depedencies are not satisfied
             call(clean_command, shell=True)
-
+            logger.warning("  loaded dependencies")
             # compile project
             compile_command = "mvn compile test-compile package -DskipTests -Drat.skip=true -Dmaven.javadoc.skip=true -Djacoco.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dcobertura.skip=true -Dpmd.skip=true -Dcpd.skip=true -Denforcer.skip=true"
-            #TODO: abort if compile is not successful
             if call(compile_command, shell=True) == 0:
-                find_compiled_logger.warning("build successful!")
-
-            # testing project
-            testing_command = "timeout -s SIGKILL 90m mvn verify -fae -Drat.skip=true -Dmaven.javadoc.skip=true -Djacoco.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dcobertura.skip=true -Dpmd.skip=true -Dcpd.skip=true -Denforcer.skip=true"
-            #TODO: abort if compile is not successful
-            find_compiled_logger.warning("starting testing!")
-            if call(testing_command, shell=True) == 0:
-                find_compiled_logger.warning("test successful!")
-
+                logger.warning("  compilation successful!")
+                # testing project
+                testing_command = "timeout -s SIGKILL 90m mvn verify -fae -Drat.skip=true -Dmaven.javadoc.skip=true -Djacoco.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dcobertura.skip=true -Dpmd.skip=true -Dcpd.skip=true -Denforcer.skip=true"
+                if call(testing_command, shell=True) == 0:
+                    logger.warning("  test successful!")
+                    #TODO: save project name and SHA
+                else:
+                    #TODO: save error log to check if this project is rescuable
+                    logger.warning("  test unsuccessful! aborting.")
+            else:
+                logger.warning("  build unsuccessful! aborting")
 
             ## clean the house for next project to come
             os.chdir(currentdir)
             rmtree(dest)
-
 
 if __name__ == "__main__":
     main()
