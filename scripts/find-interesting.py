36a5394d868220a94f2574635d92f4e8f13deabc
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2018-12-21 18:44:05 -0300

    updating find-testable.py

--- scripts/find-interesting.py
+++ scripts/find-interesting.py
@@ -8,16 +8,134 @@
 #
 # Author: Jeanderson Candido <http://jeandersonbc.github.io>, Sotero Junior <srsj2@cin.ufpe.br>
 import os, re
-
+import time
 import logging
-
+import json
 from shutil import rmtree
 from subprocess import call, check_output
+from urllib.request import urlopen, HTTPError
+from utils import CSVOutput, create_logger, SLEEP_TIME
 
-from utils import TimeInterval, CSVOutput, Query, verify_maven_support, Filter, setup_logger
+find_interesting_logger = create_logger("find-interesting","find-interesting.log")
+
+from datetime import date, timedelta
+class TimeInterval:
+    def __init__(self, since, until=None, delta=30):
+        self.starting_date = self.datefromstr(since)
+        self.ending_date = self.datefromstr(until) if until else date.today()
+        self.delta = timedelta(days=delta)
+
+    @staticmethod
+    def datefromstr(datestr):
+        fields = [int(e) for e in datestr.split("-")]
+        return date(fields[0], fields[1], fields[2])
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.starting_date > self.ending_date:
+            raise StopIteration
+        next_date = self.starting_date + self.delta
+        interval = (self.starting_date.isoformat(), next_date.isoformat())
+        self.starting_date = next_date + timedelta(days=1)
+        return interval
+
+class Query:
+    """
+    Represents a paginated query to the GitHub's Search API for repositories.
+    Query fields are represented as a key-value map like the following:
+
+        query_fields = {"language": "java", "stars": ">=1000"}
+
+    Basic Usage:
+
+        q = Query({"language": "java", "stars": ">=1000"})
+        while (q.has_next()):
+            q.fetch()
+    """
+    def __init__(self, fields):
+        self.__url_template = "https://api.github.com/search/repositories?q={query}&per_page=100&page={page}"
+        self.__fields = "+".join(["{}:{}".format(k, v) for k, v in fields.items()])
+        self.__page = 1
+        self.__max_page = Query.__request_handler(self.__get_last_page)
+
+    def has_next(self):
+        return self.__page <= self.__max_page
+
+    def fetch(self):
+        if not self.has_next():
+            raise Exception("Query has exhausted")
+        data = Query.__request_handler(self.__fetch_data)
+        self.__page += 1
+        return data
+
+    @staticmethod
+    def __request_handler(callee):
+        try:
+            return callee()
+        except HTTPError as e:
+            if e.code == 403:
+                find_interesting_logger.warning("sleeping")
+                time.sleep(SLEEP_TIME)
+                return callee()
+
+    def __get_last_page(self):
+        with urlopen(str(self)) as response:
+            header_link = response.getheader("Link")
+            if header_link is None:
+                return self.__page
+            else:
+                last_link = [link for link in header_link.split(",") if "rel=\"last\"" in link][0]
+                sub_field = re.search("&page=[0-9]+", last_link).group(0)
+                return int(re.search("[0-9]+", sub_field).group(0))
+
+    def __fetch_data(self):
+        data = {}
+        with urlopen(str(self)) as response:
+            data = json.loads(response.read().decode("utf-8"))
+        return data
+
+    def __repr__(self):
+        return self.__url_template.format(query=self.__fields, page=self.__page)
 
 
-find_interesting_logger = setup_logger("find-interesting","find-interesting.log")
+class Filter:
+    """
+    Represents a paginated query to the GitHub's Search API for repositories.
+    Query fields are represented as a key-value map like the following:
+
+        query_fields = {"language": "java", "stars": ">=1000"}
+
+    Basic Usage:
+
+        q = Query({"language": "java", "stars": ">=1000"})
+        while (q.has_next()):
+            q.fetch()
+    """
+    def __init__(self, fields):
+        self.__url_template = "https://api.github.com/search/code?q=forkcount+in:pom.xml+filename:pom.xml+repo:{query}"
+        self.__fields = fields
+
+    def fetch(self):
+        try:
+            return self.__fetch_data()
+        except HTTPError as e:
+            if e.code == 403:
+                find_interesting_logger.warning("sleeping")
+                time.sleep(SLEEP_TIME)
+                return self.__fetch_data()
+
+    def __fetch_data(self):
+        #msgLogger.warning("Checking Project {} ...".format(self.__fields))
+        data = {}
+        with urlopen(str(self)) as response:
+            data = json.loads(response.read().decode("utf-8"))
+        return data
+
+    def __repr__(self):
+        return self.__url_template.format(query=self.__fields)
+
 
 
 def checkIfInteresting(full_name, branch="master"):
--- scripts/find-testable.py
+++ scripts/find-testable.py
@@ -1,14 +1,17 @@
-import os, re, csv
-
-import logging
+import os, re, csv, logging, datetime
 
 from shutil import rmtree
 from subprocess import call, check_output
+from utils import CSVOutput, create_logger
 
-from utils import TimeInterval, CSVOutput, Query, verify_maven_support, Filter, setup_logger
+logger = create_logger("find-testable", "find-testable.log")
 
-
-logger = setup_logger("find-compiled","find-compiled.log")
+'''
+This script takes on input the file interesting.csv and produces as output 
+file testing.csv. The input file contains potential projects (containing 
+directives of test parallelization in pom.xml) whereas the output file contains
+a subset of those projects that compile and successfully run all tests.
+'''
 
 def git_revision(project_path):
     """
@@ -28,29 +31,34 @@ def git_revision(project_path):
 
 def main():
 
+    scripts_dir = os.getcwd()
     ## create download directory if it does not exit
-    output_dir = "downloads"    
-    download_dir = os.path.join(os.curdir, output_dir)
+    download_dir = "downloads"    
+    download_dir = os.path.join(os.curdir, download_dir)
     if not os.path.exists(download_dir):
         os.mkdir(download_dir)
 
+    # layout of csv file
+    output_header = ["full_name", "sha", "date"]
+    testable_csv_output= CSVOutput("testable.csv", sorted(output_header))
+
     logger.info('starting...')
     # iterating through interesting.csv rows
-    currentdir = os.getcwd()
     with open('interesting.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             full_name = row['full_name']
             branch = row['default_branch']
             depth = 1
-            dest = os.path.join(output_dir, full_name.replace(os.sep, "_"))
+            # destination directory for the project (fully-qualified name)
+            dest_dir = os.path.join(os.path.join(os.getcwd(), download_dir), full_name.replace(os.sep, "_"))
             logger.warning("processing project {}".format(full_name))
             # clone only the head of the project into download directory
-            git_cmd = "git clone http://github.com/{} -b {} --depth {} --recursive {}".format(full_name, branch, depth, dest)
+            git_cmd = "git clone http://github.com/{} -b {} --depth {} --recursive {}".format(full_name, branch, depth, dest_dir)
             call(git_cmd, shell=True)
             logger.warning("  cloned project")
             # clean project and download all dependencies
-            os.chdir(dest)
+            os.chdir(dest_dir)
             clean_command = "mvn clean dependency:go-offline"
             #TODO: check if we should interrupt if some depedencies are not satisfied
             call(clean_command, shell=True)
@@ -63,16 +71,23 @@ def main():
                 testing_command = "timeout -s SIGKILL 90m mvn verify -fae -Drat.skip=true -Dmaven.javadoc.skip=true -Djacoco.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dcobertura.skip=true -Dpmd.skip=true -Dcpd.skip=true -Denforcer.skip=true"
                 if call(testing_command, shell=True) == 0:
                     logger.warning("  test successful!")
-                    #TODO: save project name and SHA
+                    # move test reports to a file
+                    collect_test_results_command = os.path.join(scripts_dir, "move-test-reports.sh ") + dest_dir
+                    if call(collect_test_results_command, shell=True) == 0:
+                        logger.warning("  successfully collected test results!")
+                        ## save project name and sha in csv
+                        entry = {"full_name": full_name, "sha": git_revision(dest_dir), "date": datetime.date.today().strftime("%B %d, %Y")}
+                        testable_csv_output.write(entry)
+                    else:
+                        logger.warning("  could not collect test results!")
                 else:
-                    #TODO: save error log to check if this project is rescuable
                     logger.warning("  test unsuccessful! aborting.")
             else:
                 logger.warning("  build unsuccessful! aborting")
 
             ## clean the house for next project to come
-            os.chdir(currentdir)
-            rmtree(dest)
+            os.chdir(scripts_dir)
+            rmtree(dest_dir)
 
 if __name__ == "__main__":
-    main()
+   main()
